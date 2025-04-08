e410c6a2d7433c0b938ddf59040caaa75689d438
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-04-08 16:30:51 +0530

    change

---
+++ Descriptive/index.js
@@ -0,0 +1,19 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Replace with your actual Gemini API key
+const genAI = new GoogleGenerativeAI("AIzaSyA1756YkXQKxHzv5HQqW2LYkpBPvauAjlc");
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+
+  const prompt = "what is informal letter.";
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
+
+
+
