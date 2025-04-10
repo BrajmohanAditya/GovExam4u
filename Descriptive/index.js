7f1151908b4d2c9e5971b2606fda053af33ee58d
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-04-10 21:19:15 +0530

    change

--- Descriptive/index.js
+++ Descriptive/index.js
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI("AIzaSyA1756YkXQKxHzv5HQqW2LYkpBPvauAjlc");
 async function run() {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-  const prompt = "what is informal letter.";
+  const prompt = "Key of sucess.";
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
