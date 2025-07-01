a6237c9a6dfbc2f276449f9ee7e9cf68c05c73e4
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-07-01 22:59:29 +0530

    change

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+// src/pages/HomePage.jsx
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,19 @@ import React from "react";
 // import avatar from "../assets/images/tb-avatar.svg";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
+import logo from "../image/logo.jpg";
 
 export default function Navbar() {
   return (
     <nav className="w-full bg-gray-500 sticky top-0 z-[1000] px-4 py-2 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
-      <h1 className="relative left-3 font-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl text-[#ec5a47] font-serif tracking-wide pr-4">
+      {/* <h1 className="relative left-3 font-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl text-[#eb51eb] font-serif tracking-wide pr-4">
         GovExam4u
-      </h1>
+      </h1> */}
+      <img
+        src={logo}
+        alt="GovExam4u Logo"
+        className="relative left-3 h-10 sm:h-12 md:h-14 lg:h-16 pr-4"
+      />
 
       <form className="flex flex-col sm:flex-row sm:items-center gap-2 w-full sm:w-auto">
         <input
@@ -32,4 +38,4 @@ export default function Navbar() {
       </div>
     </nav>
   );
-}
+}    
