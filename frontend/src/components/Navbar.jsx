2ced8205ec0c8822b6ea27bb0eba45d9a0ba26d9
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-07-02 00:31:53 +0530

    change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,19 +3,29 @@ import React from "react";
 // import avatar from "../assets/images/tb-avatar.svg";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
-import logo from "../image/logo.jpg";
+import logo from "../assets/logo.png";
+import { GraduationCap } from "lucide-react";
+
+function Logo() {
+  return (
+    <div className="flex items-center space-x-2">
+      <GraduationCap className="h-9 w-9 text-white" />
+      <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold font-sans tracking-wide">
+        <span className="text-white">Gov</span>
+        <span className="text-orange-500">Exam4u</span>
+      </h1>
+    </div>
+  );
+}
+
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-gray-500 sticky top-0 z-[1000] px-4 py-2 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
+    <nav className="w-full  bg-blue-800 sticky top-0 z-[1000] px-4 py-2 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-2">
       {/* <h1 className="relative left-3 font-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl text-[#eb51eb] font-serif tracking-wide pr-4">
         GovExam4u
       </h1> */}
-      <img
-        src={logo}
-        alt="GovExam4u Logo"
-        className="relative left-3 h-10 sm:h-12 md:h-14 lg:h-16 pr-4"
-      />
+      <Logo />
 
       <form className="flex flex-col sm:flex-row sm:items-center gap-2 w-full sm:w-auto">
         <input
