419bc6c076060cbbe9211820d24be36499284f16
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-07-10 14:41:49 +0530

    change

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 
-// Set EJS as the template engine
+// Template engine
 app.set("view engine", "ejs");
-
-// Set views directory (IMPORTANT)
-app.set("views", path.join(__dirname, "../frontend/src/pages"));
+app.set("views", path.join(__dirname, "../frontend/src/pages")); // important
 
 app.get("/", (req, res) => {
-  res.send("hai i am root");
+  res.send("Hi i am root");
 });
 
-// Route to render the EJS file
+// descriptive route
 app.get("/descriptive", (req, res) => {
-  res.render("descriptive"); // descriptive.ejs will be picked automatically
+  res.render("descriptive");
 });
 
 app.listen(8080, () => {
