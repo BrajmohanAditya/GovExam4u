4facf398942799f2918b988cb5aca65d983c2ad7
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-07-12 16:01:01 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,46 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from "@tailwindcss/vite";
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   base: "/",
-//   server: {
-//     watch: {
-//       usePolling: true, 
-//     },
-//   },
-//   build: {
-//     outDir: "dist",
-//   },
-// });
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import fs from "fs";
-
-// Plugin to copy _redirects
-function copyRedirects() {
-  return {
-    name: "copy-redirects",
-    closeBundle() {
-      fs.copyFileSync(
-        resolve(__dirname, "public/_redirects"),
-        resolve(__dirname, "dist/_redirects")
-      );
-    },
-  };
-}
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [react(), copyRedirects()],
+  plugins: [react(), tailwindcss()],
   base: "/",
+  server: {
+    watch: {
+      usePolling: true, 
+    },
+  },
   build: {
     outDir: "dist",
   },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
 });
+
