928d9aef018b8a6f9b57ff1f05071afa4757a6d0
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-07-12 11:08:53 +0530

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,31 +1,17 @@
 
 
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import "./index.css";
-// import App from "./App.jsx";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-
-//   </StrictMode>
-// );
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // ✅ use HashRouter instead
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      {" "}
-      {/* ✅ this fixes your deployment 404 issue */}
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
+
   </StrictMode>
 );
+
