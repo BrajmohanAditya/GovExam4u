8a4dfd762ae4f29ba37544a1e1b41e14d69f5be4
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-08-19 18:40:04 +0530

    change

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,10 +1,8 @@
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8080"
-      : "https://govexam4ubackend.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
 });
 
 export default api;
