e90c686f12f0a95a0c37fe191abd6c08b5215919
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-09-22 23:28:01 +0530

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,30 @@
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "./index.css";
+
+// import AppRoutes from "./AppRoutes";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <BrowserRouter>
+//     <AppRoutes />
+//   </BrowserRouter>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 
 import AppRoutes from "./AppRoutes";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>
+  <React.StrictMode>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </React.StrictMode>
 );
 
 
