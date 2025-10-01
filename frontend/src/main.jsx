d7abdf1291d0ccd30bba7b369e6b9f508709188b
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-10-01 23:13:01 +0530

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,4 @@
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import "./index.css";
-
-// import AppRoutes from "./AppRoutes";
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <BrowserRouter>
-//     <AppRoutes />
-//   </BrowserRouter>
-// );
 
 import React from "react";
 import ReactDOM from "react-dom/client";
