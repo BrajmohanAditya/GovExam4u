063f4c0aa22e137cc636476164bf9668d8012527
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-11-10 15:33:47 +0530

    change

--- backend/controllers/user/forgotPassword.js
+++ backend/controllers/user/forgotPassword.js
@@ -15,7 +15,7 @@ const forgotPassword = async (req, res, next) => {
     const userOtp = findedUser.password_otp?.otp;
     if (userOtp) {
       const timeDiff =
-        new Date().getTime -
+        new Date().getTime() -
           new Date(findedUser.password_otp.last_attempt).getTime() <=
         24 * 60 * 60 * 1000;
       if (!timeDiff) {
@@ -31,7 +31,7 @@ const forgotPassword = async (req, res, next) => {
         throw error;
       }
     }
-    const otp = Math.floor(100000 + Math.random() * 900000) + 100000;
+    const otp = Math.floor(100000 + Math.random() * 900000);
     findedUser.password_otp.otp = otp;
     findedUser.password_otp.last_attempt = new Date();
     findedUser.password_otp.limit--;
