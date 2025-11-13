20345a4f0888084c41d9b9087435a0f08dc353ef
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-11-13 17:05:42 +0530

    change

--- frontend/src/pages/Descriptive/descriptiveRoute.jsx
+++ frontend/src/pages/Descriptive/descriptiveRoute.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import Descriptive from "./descriptive";
 import QuestionPage from "./QuestionPage";
 import { Route } from "react-router-dom";
+import ProtectedRoute from "../../protectedRoute";
 
 const DescriptiveRoutes = [
-  <Route path="/descriptive" element={<Descriptive />} key="desc" />,
-  <Route path="/questions/:type" element={<QuestionPage />} key="ques" />
+  <Route
+    path="/descriptive"
+    element={
+      <ProtectedRoute>
+        <Descriptive />
+      </ProtectedRoute>
+    }
+    key="desc"
+  />,
+
+  <Route path="/questions/:type" element={<QuestionPage />} key="ques" />,
 ];
 
 export default DescriptiveRoutes;
