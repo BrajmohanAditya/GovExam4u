72bb8a6c98f1c64bfcc215a22149980efd530004
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-11-14 14:41:07 +0530

    change

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,30 +1,3 @@
-// import nodemailer from 'nodemailer';
-// const sendEmail = async (data) => {
-//     try {
-//         const transporter = nodemailer.createTransport({
-//           service: "gmail",
-//           auth: {
-//             user: "process.env.EMAIL_USER",
-//             pass: process.env.APP_PASSWORD,
-//           },
-//         });
-//         const stringOtp = data.otp.toString();
-//         const mailOptions = {
-//           from: "process.env.EMAIL_USER",
-//           to: data.email,
-//           subject: "password reset otp",
-//           text: stringOtp,
-//         };
-//         const result = await transporter.sendMail(mailOptions);
-//         console.log('Email sent successfully:', result);
-//         return result;
-
-//     }catch (error) {
-//         console.log( error);
-//     }
-// }
-
-// export default sendEmail;
 
 import { Resend } from "resend";
 
