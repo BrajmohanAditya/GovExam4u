9f640037dbffdd569c79ed0cb7a3aa694d33be98
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-11-26 17:13:00 +0530

    change

--- frontend/src/pages/LiveMock/LiveMockRouter.jsx
+++ frontend/src/pages/LiveMock/LiveMockRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import LiveMock from "./HomeLiveMock";
+import LiveMock from "./Home";
 
 const LiveMockRouter = [<Route path="/livemock" element={<LiveMock />} />];
 
