d5e1b89916fa9e7f934755187583f26a8080a380
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-12-07 17:42:07 +0530

    change

--- frontend/src/pages/TO-DO-List/todoListRoute.jsx
+++ frontend/src/pages/TO-DO-List/todoListRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Home from './home'
 
-import TodoApp from './page'
+import TodoApp from './Home'
 const todoListRoute = [
   // <Route path="/todo-list" element={<Home />} />,
   <Route path="/todo-list" element={<TodoApp />} />,
