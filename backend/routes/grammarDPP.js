4b6b2e85ec3cd9e1edb0706fe84bc4a39b4ffddf
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-01-03 01:02:36 +0530

    change

--- backend/routes/grammarDPP.js
+++ backend/routes/grammarDPP.js
@@ -1,10 +1,11 @@
 import express from "express";
 import  addQuiz  from "../controllers/grammarDPP/addQuize.js";
 import  getQuiz  from "../controllers/grammarDPP/getQuiz.js";
-
+import auth from "../middlewares/auth.js";
+import adminRoles from "../middlewares/adminRole.js";
 const router = express.Router();
 
-router.post("/addQuize",  addQuiz);
+router.post("/addQuize", auth, adminRoles("editor", "admin"), addQuiz);
 router.get("/getQuiz",  getQuiz);
 
 export default router; 
