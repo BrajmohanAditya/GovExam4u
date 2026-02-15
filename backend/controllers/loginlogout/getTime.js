d3be1eda43784c2900778797269d1886d7d2179f
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-02-15 12:28:50 +0530

    change

--- backend/controllers/loginlogout/getTime.js
+++ backend/controllers/loginlogout/getTime.js
@@ -1,15 +1,25 @@
 import User from "../../models/loginLogout.js";
+import RegisterOtp from "../../models/registerOtp.js";
 
 const getTime = async (req, res, next) => {
   const { email } = req.body;
   try {
+    let time = null;
     const findedUser = await User.findOne({ email: email });
-    if (!findedUser) {
+    if (findedUser) {
+      time = findedUser?.password_otp?.send_time || null;
+    } else {
+      // check temporary registration OTP store
+      const reg = await RegisterOtp.findOne({ email: email });
+      if (reg) {
+        time = reg.send_time || null;
+      }
+    }
+    if (!time) {
       const error = new Error("User not found");
       error.status = 404;
       throw error;
     }
-    const time = findedUser?.password_otp?.send_time || null;
     res.status(200).json({ message: "otp sent", status: true, time });
   } catch (error) {
     next(error);
