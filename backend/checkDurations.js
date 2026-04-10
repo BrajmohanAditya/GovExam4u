422605e6eaa0eea892f65009175c1c29f56a5d1a
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-04-10 23:27:33 +0530

    change

--- backend/checkDurations.js
+++ backend/checkDurations.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
-import SubmittedTest from "./models/BankingQuiz/submittedTest.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 async function run() {
   await mongoose.connect(process.env.ATLASDB_URL);
-  const res = await SubmittedTest.deleteMany({});
-  console.log("Deleted tests:", res.deletedCount);
+  const res = await mongoose.connection.db.collection('times').find({}).toArray();
+  console.log("Times collection:", res);
   process.exit(0);
 }
 
